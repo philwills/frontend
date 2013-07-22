7ee67f73f014d6d49c236f845323d61035b3ee3c
Grant Klopper <grant.klopper@guardian.co.uk>
2013-07-22 17:01:13 +0100

    Wired up Highlighted comments page

--- discussion/app/controllers/HighlightedCommentsController.scala
+++ discussion/app/controllers/HighlightedCommentsController.scala
@@ -6,16 +6,29 @@ import discussion.DiscussionApi
 import model._
 
 
-object HighlightedCommentsController extends Controller with Logging with ExecutionContexts {
+object HighlightedCommentsController extends Controller with Logging with ExecutionContexts with implicits.Requests {
 
+  def renderJson() = render()
   def render() = Action { implicit request =>
     val promiseOfHighlightedComments = DiscussionApi.highlightedComments()
 
     Async {
       promiseOfHighlightedComments.map{ highlightedComments =>
+
+        if (request.isJson) {
+
+          highlightedComments.comments.headOption.map{ comment =>
+            Cached(60){
+              JsonComponent(views.html.fragments.comment(comment))
+            }
+          }.getOrElse(NotFound)
+
+
+        } else {
+
         Cached(600){
             Ok(views.html.highlightedComments(highlightedComments))
-        }
+        }       }
       }
     }
   }
