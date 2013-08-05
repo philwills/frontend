e369db81993667682692fb82107b5e8a441ae260
David Vella <david.vella@guardian.co.uk>
2013-08-05 15:12:50 +0100

    Adding JS to allow ad placements

---
+++ common/app/assets/javascripts/modules/adverts/ad-placement-test.js
@@ -0,0 +1,29 @@
+define(['common', 'bean', 'modules/userPrefs'], function (common, bean, userPrefs) {
+
+    /*
+        #gu.prefs.adplacement=inline-5
+        #gu.prefs.adplacement=inline-7
+     */
+
+    function AdPlacementTest(config, context) {
+        var adplacement = userPrefs.get('adplacement').split('-'),
+            adType      = adplacement[0];
+
+        this.context = context;
+
+        switch(adType) {
+            case 'inline': this.insertInlineAds(adplacement[1]); break;
+        }
+    }
+
+    AdPlacementTest.prototype.insertInlineAds = function(interval) {
+        var advertHtml  = '<div class="ad-slot ad-slot--placeholder">'
+                        + '  <div class="ad-container">ADVERT</div>'
+                        + '</div>';
+
+        common.$g('.article-body p:nth-of-type('+interval+'n)', this.context).after(advertHtml);
+    };
+
+
+    return AdPlacementTest;
+});
