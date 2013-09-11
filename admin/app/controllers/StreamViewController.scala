22dffc4a452a70d3d9bc2dc07d81fe3fab2b10f4
David Vella <dave@deepcobalt.com>
2013-09-11 06:57:25 +0100

    Adding JSON endpoint (sortof)

--- admin/app/controllers/StreamViewController.scala
+++ admin/app/controllers/StreamViewController.scala
@@ -3,6 +3,8 @@ package controllers
 import play.api.mvc.Controller
 import common.Logging
 import tools.CloudWatch
+import play.api.libs.json._
+import tools.Chart
 
 object StreamViewController extends Controller with Logging with AuthLogging {
   def render() = AuthAction{ request =>
@@ -12,5 +14,17 @@ object StreamViewController extends Controller with Logging with AuthLogging {
                                CloudWatch.healthyHostEuWest1aCount,
                                CloudWatch.healthyHostEuWest1bCount
                                ))
+  }
+
+  def renderJson() = AuthAction { request =>
+
+    val response = Map(
+      "requestOkCount" -> CloudWatch.requestOkCount.toString,
+      "fastlyStats"    -> CloudWatch.fastlyHitMissStatistics.toString,
+      "instances"      -> CloudWatch.healthyHostEuWest1aCount.toString
+    )
+
+    Ok(Json.toJson(response)).as("application/javascript")
+
   }
 }
